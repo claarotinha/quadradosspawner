7bc40b6d2b13a3020ee936cb7ff8ced9867a7ae6
claarotinha <clar4costa705@gmail.com>
2025-04-30 07:42:10 -0300

    colisao
    colisao ebaaaaa

--- My project/Assets/Scripts/colisao.cs
+++ My project/Assets/Scripts/colisao.cs
@@ -1,9 +1,17 @@
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class colisao : MonoBehaviour
+public class Moreu : MonoBehaviour
 {
-    void OnCollisionEnter (Collision2D colisao){
-        Debug.Log("Game Over!!!"+ colisao. gameObject.name);
-
-    }
+private void OnTriggerEnter2D(Collider2D other)
+{
+if (other.CompareTag("Player"))
+{
+Debug.Log("Game Over");
+SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reinicia a cena
+}
+}
 }
